8fd03508a65fc5e55d508d3a812e54aa7b6183c4
Alfonso Gálvez <Alfonso.galvez@juntadeandalucia.es>
2023-08-29 13:47:21 +0200

    Limpieza de proyecto para partir del proyecto generado por spring boot initializar

--- src/com/agapa/pleamar/boyasChicas/InicioSpringApplication.java
+++
@@ -1,8 +0,0 @@
-package com.agapa.pleamar.boyasChicas;
-@SpringBootApplication
-public class InicioSpringApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(InicioSpringApplication.class, args);
-    }
-}
