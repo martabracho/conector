b550a5b582fb895106b76fb55a0913517662ac4f
Alfonso Gálvez <Alfonso.galvez@juntadeandalucia.es>
2023-08-29 14:01:46 +0200

    Inicializacion a partir de la web de spring

---
+++ src/main/java/es/juntadeandalucia/agapa/pleamar/ckan_conector_ws/CkanConectorWsApplication.java
@@ -0,0 +1,13 @@
+package es.juntadeandalucia.agapa.pleamar.ckan_conector_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkanConectorWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkanConectorWsApplication.class, args);
+	}
+
+}
